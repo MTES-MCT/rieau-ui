c3058448cb360d67df39a6f20581bdfe803d0057
Tristan Robert <tristan.robert.44@gmail.com>
2019-05-20 13:19:16 +0200

    Amélioration page communes

--- src/pages/Communes/index.js
+++ src/pages/Communes/index.js
@@ -9,28 +9,25 @@ import mours from './mours.jpg';
 import nogentSurOise from './nogent-sur-oise.jpg';
 import saintTropez from './saint-tropez.jpg';
 import Container from '@material-ui/core/Container';
-import IconButton from '@material-ui/core/IconButton';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '../../components/Typography';
 import Button from '../../components/Button';
 import AppFooter from '../../components/AppFooter';
 import AppAppBar from '../../components/AppAppBar';
 import withRoot from '../../theme/withRoot';
-import { FaGlobe } from 'react-icons/fa';
-import LazyLoad from 'react-lazyload';
 
 const styles = theme => ({
   root: {
+    display: 'flex'
+  },
+  images: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper
+    minWidth: 300,
+    width: '100%'
   },
   title: {
-    marginBottom: theme.spacing(14)
+    marginBottom: theme.spacing(10)
   },
   container: {
     position: 'relative',
@@ -38,60 +35,121 @@ const styles = theme => ({
     display: 'flex',
     alignItems: 'center'
   },
-  gridList: {
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)'
+  image: {
+    position: 'relative',
+    height: 200,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15
+      },
+      '& $imageMarked': {
+        opacity: 0
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor'
+      }
+    }
   },
-  imgTitle: {
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     color: theme.palette.common.white
   },
-  titleBar: {
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%'
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity')
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+      6}px`
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity')
   },
   button: {
-    marginTop: theme.spacing(8)
+    margin: theme.spacing(10)
   }
 });
 
 function Communes(props) {
   const { classes } = props;
 
-  const tileData = [
+  const images = [
     {
-      img: andeville,
+      file: andeville,
       title: 'Andeville',
-      url: 'https://www.andeville.fr/'
+      url: 'https://www.andeville.fr/',
+      width: '50%'
     },
     {
-      img: baziege,
+      file: baziege,
       title: 'Baziège',
-      url: 'https://www.ville-baziege.fr'
+      url: 'https://www.ville-baziege.fr',
+      width: '50%'
     },
     {
-      img: castanetTolosan,
+      file: castanetTolosan,
       title: 'Castanet-Tolosan',
-      url: 'https://www.castanet-tolosan.fr'
+      url: 'https://www.castanet-tolosan.fr',
+      width: '50%'
     },
     {
-      img: laCreche,
+      file: laCreche,
       title: 'La Crèche',
-      url: 'https://www.ville-lacreche.fr'
+      url: 'https://www.ville-lacreche.fr',
+      width: '50%'
     },
     {
-      img: mours,
+      file: mours,
       title: 'Mours',
-      url: 'http://www.ville-mours.fr/'
+      url: 'http://www.ville-mours.fr/',
+      width: '50%'
     },
     {
-      img: nogentSurOise,
+      file: nogentSurOise,
       title: 'Nogent-sur-Oise',
-      url: 'www.nogentsuroise.fr'
+      url: 'http://www.nogentsuroise.fr',
+      width: '50%'
     },
     {
-      img: saintTropez,
+      file: saintTropez,
       title: 'Saint-Tropez',
-      url: 'https://www.saint-tropez.fr'
+      url: 'https://www.saint-tropez.fr',
+      width: '100%'
     }
   ];
 
@@ -108,33 +166,44 @@ function Communes(props) {
           >
             {`Communes partenaires`}
           </Typography>
-          <div className={classes.root}>
-            <GridList className={classes.gridList} cols={3}>
-              {tileData.map(tile => (
-                <GridListTile key={tile.img}>
-                  <LazyLoad>
-                    <img src={tile.img} alt={tile.title} />
-                  </LazyLoad>
-                  <GridListTileBar
-                    title={tile.title}
-                    classes={{
-                      root: classes.titleBar,
-                      title: classes.imgTitle
-                    }}
-                    actionIcon={
-                      <IconButton href={tile.url}>
-                        <FaGlobe className={classes.imgTitle} />
-                      </IconButton>
-                    }
-                  />
-                </GridListTile>
-              ))}
-            </GridList>
+          <div className={classes.images}>
+            {images.map(image => (
+              <ButtonBase
+                focusRipple
+                key={image.title}
+                className={classes.image}
+                focusVisibleClassName={classes.focusVisible}
+                style={{
+                  width: image.width
+                }}
+                href={image.url}
+              >
+                <span
+                  className={classes.imageSrc}
+                  style={{
+                    backgroundImage: `url(${image.file})`
+                  }}
+                />
+                <span className={classes.imageBackdrop} />
+                <span className={classes.imageButton}>
+                  <Typography
+                    component="span"
+                    variant="subtitle1"
+                    color="inherit"
+                    className={classes.imageTitle}
+                  >
+                    {image.title}
+                    <span className={classes.imageMarked} />
+                  </Typography>
+                </span>
+              </ButtonBase>
+            ))}
           </div>
           <Button
             color="secondary"
             size="large"
             variant="contained"
+            className={classes.button}
             href="mailto:ads-relations-collectivites@developpement-durable.gouv.fr?Subject=Rejoindre%20permis%20construire%20facile"
           >
             {`Rejoignez-nous`}
