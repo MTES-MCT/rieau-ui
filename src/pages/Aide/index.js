48a90ef508484f14dcae654d804e2e06a4345ac6
Tristan Robert <tristan.robert.44@gmail.com>
2019-05-20 09:34:32 +0200

    Fix aide et cgu

--- src/pages/Aide/index.js
+++ src/pages/Aide/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import withRoot from '../../theme/withRoot';
-import Markdown from '../../components/Markdown';
 import Typography from '../../components/Typography';
 import AppAppBar from '../../components/AppAppBar';
-/* eslint import/no-webpack-loader-syntax: off */
-import faq from '!raw-loader!./faq.md';
 import AppFooter from '../../components/AppFooter';
+import { importMDX } from 'mdx.macro';
+
+const Faq = lazy(() => importMDX('./faq.mdx'));
 
 function Help() {
   return (
@@ -18,7 +18,9 @@ function Help() {
           <Typography variant="h3" gutterBottom marked="center" align="center">
             Aide
           </Typography>
-          <Markdown>{faq}</Markdown>
+          <Suspense fallback={<div>Chragement en cours...</div>}>
+            <Faq />
+          </Suspense>
         </Box>
       </Container>
       <AppFooter />
--- src/pages/Cgu/index.js
+++ src/pages/Cgu/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import withRoot from '../../theme/withRoot';
-import Markdown from '../../components/Markdown';
 import Typography from '../../components/Typography';
 import AppAppBar from '../../components/AppAppBar';
-/* eslint import/no-webpack-loader-syntax: off */
-import cgu from '!raw-loader!./cgu.md';
 import AppFooter from '../../components/AppFooter';
+import { importMDX } from 'mdx.macro';
+
+const Cgu = lazy(() => importMDX('./cgu.mdx'));
 
 function Help() {
   return (
@@ -18,7 +18,9 @@ function Help() {
           <Typography variant="h3" gutterBottom marked="center" align="center">
             CGU
           </Typography>
-          <Markdown>{cgu}</Markdown>
+          <Suspense fallback={<div>Chragement en cours...</div>}>
+            <Cgu />
+          </Suspense>
         </Box>
       </Container>
       <AppFooter />
