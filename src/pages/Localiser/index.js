64955299fdadd748d9eb10fd07c03ba997156aae
Tristan Robert <tristan.robert.44@gmail.com>
2019-05-21 12:21:27 +0200

    Ajout de la carte de localisation du projet

--- src/pages/Localiser/index.js
+++ src/pages/Localiser/index.js
@@ -6,21 +6,32 @@ import withRoot from '../../theme/withRoot';
 import AppFooter from '../../components/AppFooter';
 import AppAppBar from '../../components/AppAppBar';
 import compose from '../../utils/compose';
+
 const styles = theme => ({
   map: {
     display: 'flex',
-    height: '400px'
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important',
+      height: 200
+    },
+    height: 800,
+    width: '100%'
   }
 });
 
 const basemapUrl = 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png';
-const villes = {
-  Andeville: { lat: 48.852969, lon: 2.349903 },
-  Baziège: { lat: 48.383, lon: -4.5 }
-};
+const communes = [
+  { nom: 'Andeville', position: [49.25, 2.1667] },
+  { nom: 'Baziège', position: [43.45, 1.6167] },
+  { nom: 'Mours', position: [49.1333, 2.2667] },
+  { nom: 'Nogent-sur-Oise', position: [49.2667, 2.4667] },
+  { nom: 'La Crèche', position: [46.3667, -0.3] },
+  { nom: 'Castanet-Tolosan', position: [43.5167, 1.5] },
+  { nom: 'Saint-Tropez', position: [43.2667, 6.6333] }
+];
 class Localiser extends React.Component {
   state = {
-    position: [48.852969, 2.349903]
+    position: [45.9167, 1.8833]
   };
   render() {
     const { classes } = this.props;
@@ -28,33 +39,21 @@ class Localiser extends React.Component {
     return (
       <React.Fragment>
         <AppAppBar />
-        <div className={classes.map}>
-          <LeafletMap
-            center={position}
-            zoom={6}
-            maxZoom={10}
-            attributionControl={true}
-            zoomControl={true}
-            doubleClickZoom={true}
-            scrollWheelZoom={true}
-            dragging={true}
-            animate={true}
-            easeLinearity={0.35}
-          >
-            <TileLayer
-              attribution={`données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>`}
-              url={basemapUrl}
-              opacity={0.3}
-              zIndex={1}
-            />
-            <MarkerList />
-            <Marker position={position}>
+        <LeafletMap center={position} zoom={6} className={classes.map}>
+          <TileLayer
+            attribution={`données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>`}
+            url={basemapUrl}
+          />
+          {communes.map(commune => (
+            <Marker key={commune.nom} position={commune.position}>
               <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                {`Commune partenaire:`}
+                <br />
+                {commune.nom}
               </Popup>
             </Marker>
-          </LeafletMap>
-        </div>
+          ))}
+        </LeafletMap>
         <AppFooter />
       </React.Fragment>
     );
