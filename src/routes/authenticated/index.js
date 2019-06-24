5832ebb762df1b852c5269ce064767345c119bdc
Tristan Robert <tristan.robert.44@gmail.com>
2019-06-24 09:32:20 +0200

    Fix des pages Aide et CGU en mode authentifié

--- src/routes/authenticated/index.js
+++ src/routes/authenticated/index.js
@@ -7,6 +7,9 @@ import Communes from 'pages/Communes';
 import MapIcon from '@material-ui/icons/Map';
 import SaveAltIcon from '@material-ui/icons/SaveAlt';
 import CityIcon from '@material-ui/icons/LocationCity';
+import Cgu from 'pages/Cgu';
+import Aide from 'pages/Aide';
+import AideIcon from '@material-ui/icons/Help';
 
 const routes = [
   {
@@ -48,6 +51,22 @@ const routes = [
     icon: SignOutIcon,
     sidebar: true,
     auth: true
+  },
+  {
+    id: 'aide',
+    path: '/aide',
+    label: 'Aide',
+    icon: AideIcon,
+    component: Aide,
+    sidebar: false
+  },
+  {
+    id: 'cgu',
+    path: '/cgu',
+    label: 'CGU',
+    icon: null,
+    component: Cgu,
+    sidebar: false
   }
 ];
 
