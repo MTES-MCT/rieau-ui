42bda4229938ec93e40cc01fe03a171675e0241a
Tristan Robert <tristan.robert.44@gmail.com>
2019-06-27 13:09:53 +0200

    Ajout d'une taille maximale de fichier téléversé

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -5,12 +5,17 @@ import RootRef from '@material-ui/core/RootRef';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core';
 import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 import withMobileDialog from '@material-ui/core/withMobileDialog';
 import Button from 'components/Button';
 import compose from 'utils/compose';
+import { formatFileSize } from 'utils/files';
+import Typography from './Typography';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const styles = theme => ({
   dropzone: {
@@ -19,9 +24,19 @@ const styles = theme => ({
     borderWidth: 2,
     borderColor: 'rgb(102, 102, 102)',
     borderStyle: 'dashed',
-    borderRadius: 5
+    borderRadius: 5,
+    padding: theme.spacing(1)
+  },
+  appBar: {
+    position: 'relative'
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1
   }
 });
+
+const maxSize = parseInt(process.env.REACT_APP_MAX_SIZE_UPLOAD_FILE);
 
 function UploadFile(props) {
   const { handleFile, onClose, fullScreen, classes, pieceJointe } = props;
@@ -41,9 +56,14 @@ function UploadFile(props) {
     },
     [handleFile, pieceJointe]
   );
-  const { getRootProps, getInputProps, isDragReject } = useDropzone({
+  const {
+    getRootProps,
+    getInputProps,
+    isDragReject,
+    rejectedFiles
+  } = useDropzone({
     accept: pieceJointe.formats,
-    maxSize: 1024 * 1024,
+    maxSize: maxSize,
     onDrop: onDrop
   });
   const { ref, ...rootProps } = getRootProps();
@@ -51,6 +71,12 @@ function UploadFile(props) {
     onClose();
     setShowDialog(false);
   }
+
+  const rejectedFilesItems = rejectedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {formatFileSize(file.size)}
+    </li>
+  ));
 
   return (
     <Dialog
@@ -60,18 +86,43 @@ function UploadFile(props) {
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">{`Ajouter un fichier`}</DialogTitle>
+      <AppBar className={classes.appBar} color="secondary">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            {`Téléverser un fichier`}
+          </Typography>
+          <IconButton
+            edge="end"
+            color="inherit"
+            onClick={handleClose}
+            aria-label="Close"
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <DialogContent>
         <RootRef rootRef={ref}>
           <Paper {...rootProps} className={classes.dropzone}>
             <input {...getInputProps()} />
             {isDragReject ? (
-              <p>{`Ce fichier n'est pas au bon format (pdf ou image)`}</p>
+              <p>{`Ce fichier n'est pas acceptable`}</p>
             ) : (
-              <p>
-                {`Cliquez ici pour sélectionner un fichier ou glissez puis déposez-le ici`}
-              </p>
+              <React.Fragment>
+                <p>
+                  {`Cliquez ici pour sélectionner un fichier ou glissez-le ici`}
+                </p>
+                {rejectedFilesItems.length > 0 && (
+                  <aside>
+                    <h4 style={{ color: 'red' }}>Fichier rejeté:</h4>
+                    <ul>{rejectedFilesItems}</ul>
+                  </aside>
+                )}
+              </React.Fragment>
             )}
+            <em>{`(Seuls les fichiers image et pdf de moins de ${formatFileSize(
+              maxSize
+            )} sont acceptés)`}</em>
           </Paper>
         </RootRef>
       </DialogContent>
