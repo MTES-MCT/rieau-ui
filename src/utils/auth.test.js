41bc294f4e167fb31020eb277fb51ddd74067958
Tristan Robert <tristan.robert.44@gmail.com>
2019-06-28 11:13:52 +0200

    Fix authentication test

--- src/utils/auth.test.js
+++ src/utils/auth.test.js
@@ -2,12 +2,12 @@ import auth from 'utils/auth';
 import users from './users-mock';
 
 test('signin ok', async () => {
-  await auth.login();
+  await auth.login('jean.martin');
   expect(await auth.isAuthenticated()).toBe(true);
   expect(await auth.getUser()).toBe(users[0]);
 });
 test('signout ok', async () => {
-  await auth.login();
+  await auth.login('jean.martin');
   await auth.logout();
   expect(await auth.isAuthenticated()).toBe(false);
 });
