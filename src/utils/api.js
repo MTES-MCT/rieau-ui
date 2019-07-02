fcfaa627ac9f1ca4cf2eeaf49714e106fa7b0356
Tristan Robert <tristan.robert.44@gmail.com>
2019-07-02 08:44:59 +0200

    Fix api mock env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 const isApiMock =
   process.env.REACT_APP_API_MOCK === true ||
-  process.env.REACT_APP_API_MOCK === 'true';
+  process.env.REACT_APP_API_MOCK === 'true' ||
+  process.env.REACT_APP_API_MOCK === 1 ||
+  process.env.REACT_APP_API_MOCK === '1';
 
 function api() {
   const importApi = isApiMock
