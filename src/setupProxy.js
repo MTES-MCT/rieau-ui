f67bdd65dc0e0d328d0f4755e75c9190e4f1c11e
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-09 11:45:20 +0200

    Extraction des variables env de index.html

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-import { env } from 'utils/env-helper';
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(
-    proxy('/api', { target: env('REACT_APP_API_URL'), changeOrigin: true })
-  );
-};
