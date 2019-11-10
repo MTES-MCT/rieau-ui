48662d494118ce810f100379cc01faecd32712ba
Tristan Robert <tristan.robert.44@gmail.com>
2019-11-10 17:01:41 +0100

    feature: error dark theme et retour

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,24 +3,62 @@ import PropTypes from 'prop-types';
 import Typography from 'components/Typography';
 import AppAppBar from 'components/AppAppBar';
 import AppFooter from 'components/AppFooter';
+import AppTheme from 'theme/AppTheme';
+import { withRouter } from 'react-router-dom';
+import Button from 'components/Button';
+import BackIcon from '@material-ui/icons/ArrowBack';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/styles';
+import compose from 'utils/compose';
+
+const styles = theme => ({
+  grid: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+});
 
 function Error(props) {
-  const { error } = props;
+  const { classes, error, history } = props;
   return (
-    <React.Fragment>
+    <AppTheme>
       <AppAppBar />
-      <Typography variant="h3" marked="center" align="center">
-        {`Erreur`}
-      </Typography>
-      <Typography variant="body1" marked="center" align="center">
-        {error.message}
-      </Typography>
+      <Grid container className={classes.grid}>
+        <Grid item xs={12}>
+          <Typography variant="h3" marked="center" align="center">
+            {`Erreur`}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1" marked="center" align="center">
+            {error.message}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="secondary"
+            aria-label="Retour"
+            data-cy="back-btn"
+            onClick={event => history.push('/')}
+          >
+            <BackIcon />
+            {`Retour`}
+          </Button>
+        </Grid>
+      </Grid>
       <AppFooter />
-    </React.Fragment>
+    </AppTheme>
   );
 }
 Error.propTypes = {
-  error: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  error: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 
-export default Error;
+export default compose(
+  withRouter,
+  withStyles(styles)
+)(Error);
